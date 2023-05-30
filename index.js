50603a4b2ffca8b904872e1d7c965778a61d2db1
isha-web1 <isha.web1@gmail.com>
2023-05-30 10:40:16 +0600

    menu and reviews send to client

--- index.js
+++ index.js
@@ -28,12 +28,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCollection = client.db('bistro-DB').collection('menu');
+    const reviewsCollection = client.db('bistro-DB').collection('reviews');
+
+    app.get('/menu', async (req, res) => {
+        const result = await menuCollection.find().toArray();
+        res.send(result)
+    })
+    app.get('/reviews', async (req, res) => {
+        const result = await reviewsCollection.find().toArray();
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
